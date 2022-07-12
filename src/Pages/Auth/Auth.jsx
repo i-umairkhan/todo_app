a8002c680e05ed77735f1e634eb07952e86d6cc1
umair2607 <umair.k2607@gmail.com>
2022-07-13 01:11:05 +0500

    added divider

--- src/Pages/Auth/Auth.jsx
+++ src/Pages/Auth/Auth.jsx
@@ -3,11 +3,15 @@ import Login from "../../Component/Login/Login";
 import "./Auth.scss";
 import React from "react";
 import { Link } from "react-router-dom";
+import { Chip, Divider } from "@mui/material";
 
 const Auth = () => {
   return (
     <div className="auth">
       <SignUp />
+      <Divider orientation="vertical" variant="middlei">
+        <Chip label="TODO APP" />
+      </Divider>
       <Login />
     </div>
   );
