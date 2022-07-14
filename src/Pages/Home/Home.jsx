e7311f67326527034c9be50971c878001434782e
umair2607 <umair.k2607@gmail.com>
2022-07-15 00:56:43 +0500

    list functionality

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,12 +1,47 @@
 import "./Home.scss";
 
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Button, TextField } from "@mui/material";
 
 const Home = () => {
+  const [todoInput, setTodoInput] = useState("");
+  const [todoList, setTodoList] = useState([]);
+  const onAddHandler = () => {
+    todoInput
+      ? setTodoList((pre) => [...pre, todoInput])
+      : setTodoList((pre) => [...pre]);
+    setTodoInput("");
+  };
   return (
-    <div>
-      <Link to="/">Home</Link>
+    <div className="home">
+      <h1>todo app</h1>
+      <div className="home__input">
+        <TextField
+          id="filled-basic"
+          label="Enter todo"
+          variant="filled"
+          value={todoInput}
+          autoComplete="off"
+          onChange={(event) => setTodoInput(event.target.value)}
+        />
+        <Button
+          variant="contained"
+          className="add__button"
+          onClick={onAddHandler}
+        >
+          +
+        </Button>
+      </div>
+      <div className="list">
+        {todoList.map((item) => {
+          return (
+            <div className="list__item" key={todoList.indexOf(item)}>
+              {todoList.indexOf(item) + 1} {` . `}
+              {item}
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
